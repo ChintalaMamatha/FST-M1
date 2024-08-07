4fb799f435c16f87bd2aa3b6327845dd735ec87a
ChintalaMamatha <cmamath1@in.ibm.com>
2024-08-07 17:00:07 +0530

    Add files via upload

--- Cucumber/Activities/TestRunner.java
+++ Cucumber/Activities/TestRunner.java
@@ -6,17 +6,18 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/java/Features",
-		glue = {"stepDefinitions"} ,
-		tags = "@Activity5",
-		publish = true,
-		plugin = {
-				"pretty",
+		features ="src/test/java/Features",
+		glue={"stepDefinitions"},
+		tags="@activity5",
+		publish=true,
+		plugin= {"pretty",
 				"html:src/reports/HTML_Report.html",
 				"json:src/reports/JSON_Report.json",
 				"junit:src/reports/XML_Report.xml"
-		}
-		)
+}
+	
+)
+
 
 public class TestRunner {
 
